eb678963361331a960f2152834e2afdfa3e4af4d
JPX87 <jeremy.patapy@gmail.com>
2026-02-12 00:08:07 +0100

    Test auto Deploy2

--- src/app/(header)/page.tsx
+++ src/app/(header)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="relative w-10/12 flex flex-col gap-3 py-14 px-6 justify-center rounded-3xl bg-(--color-tertiary) text-(--color-secondary) dark:text-(--color-seconde-black) m-auto">
           <h1 className="text-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold my-[50px] transition-all">DES QUESTIONS SUR LES JNM ?</h1>
           <Link target="_blank" href="https://ig.me/m/jnm2026_tls" rel="noopener noreferrer" className="block m-auto w-max bg-(--color-secondary) dark:bg-(--color-seconde-black) text-(--color-tertiary) text-xl sm:text-2xl font-bold rounded-4xl py-2 sm:py-3 px-8 sm:px-10 hover:bg-gray-800 transition-all cursor-pointer">
-            CONTACTEZ-NOUS ^^
+            CONTACTEZ-NOUS
           </Link>
           <MessageBubble classNameP="absolute top-[-20px] md:top-[-50px] right-5" className="bg-(--color-tertiary) border-(--color-primary) border-4" rounded />
           <MessageBubble classNameP="absolute bottom-[-30px] md:bottom-[-50px] left-5" className="bg-(--color-tertiary) border-(--color-primary) border-4" />
