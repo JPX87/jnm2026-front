a012eb7f5c4065f83a24448ca76d4ffc57c78e42
ghelg <95490983+guillaume-helg@users.noreply.github.com>
2026-02-24 14:06:44 +0100

    feat: loader when changing page (#12)
    * fix: delete navigation_loader.scss
    * fix: imports

--- src/components/layout/PageLayout/PageLayout.tsx
+++ src/components/layout/PageLayout/PageLayout.tsx
@@ -2,9 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { Oswald, Open_Sans } from "next/font/google";
 import '@/scss/globals.scss'
 import HeaderGrid from "@/components/layout/Header/HeaderGrid";
-import Footer from "@/components/layout/Footer/Footer";
+import NavigationLoader from "@/components/ui/NavigationLoader/NavigationLoader";
 import { ThemeProvider } from "@/components/ui/theme/ThemeProvider/ThemeProvider";
-import ScrollToHash from "@/lib/ScrollToHash";
 
 // Configuration des polices
 const oswald = Oswald({
@@ -81,10 +80,9 @@ export default function RootLayout({
           enableSystem
           themes={["light", "dark"]}
         >
-          <ScrollToHash />
+          <NavigationLoader />
           {header && <HeaderGrid />}
           {children}
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
