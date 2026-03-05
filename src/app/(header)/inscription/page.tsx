790b6058723540cc5057e1c4d89e06d2685e92f3
ghelg <95490983+guillaume-helg@users.noreply.github.com>
2026-03-05 16:48:35 +0100

    Add metadata for Inscription page

--- src/app/(header)/inscription/page.tsx
+++ src/app/(header)/inscription/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next';
 import InscriptionContent from "@/components/features/InscriptionContent/InscriptionContent";
 
+export const metadata: Metadata = { 
+    title: "Inscription", 
+}
 export default function InscriptionPage() {
     return <InscriptionContent />;
 }
